6442b376490ed2a255065a4247152916d6976d3c
Anvarbek Toshmamatov <anvarbektoshmamatov888@gmail.com>
2023-06-06 07:46:59 +0500

    js1

---
+++ js/js1/index.js
@@ -0,0 +1,47 @@
+// console.log('Hello world', 10+20,'salom');
+
+// var text1="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, architecto accusantium neque officiis eveniet, et eius dolor consectetur ab aliquam explicabo molestiae qui suscipit. Id unde corrupti optio beatae aliquid?"
+
+// let text="lorem lorem lorem"
+
+// console.log("Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, architecto accusantium neque officiis eveniet, et eius dolor consectetur ab aliquam explicabo molestiae qui suscipit. Id unde corrupti optio beatae aliquid?")
+
+// console.log(text)
+// console.log(text1)
+// console.log(text)
+// console.log(text)
+
+
+// let lastName="Toshmamatov"
+// let firstName=`Anvarbek`
+// console.log(lastName,firstName)
+
+
+// let isMarrid=true
+// console.log(!isMarrid,isMarrid)
+// let n=10.2
+// console.log(n)
+
+// let nom=undefined
+// console.log(nom)
+// let nom1
+// console.log(nom1)
+
+// let a=20
+// let b=10
+// console.log(`${a} bilan ${b} ning yig'indisi: ${a+b}`)
+
+
+// let a=10
+// let b=20
+// b=a+b
+// a=b-a
+// b=b-a
+// console.log(a)
+// console.log(b)
+
+const n=345
+const m=2345
+console.log((n-n%100)/100+(n%100-n%10)/10+n%10)
+console.log(parseInt(n/100)+parseInt((n%100)/10)+n%10)//3-xonali son uchun raqamlar yigindisi
+console.log(parseInt(m/1000)+parseInt(m%1000/100)+parseInt((m%100)/10)+m%10) //4-xonali sonni raqamlar yigindisi

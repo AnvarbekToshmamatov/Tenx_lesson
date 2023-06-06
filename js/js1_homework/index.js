a68e3d0b46eddc9a7df7f795454805e353945476
Anvarbek Toshmamatov <anvarbektoshmamatov888@gmail.com>
2023-06-06 08:15:13 +0500

    js1-dars homework

---
+++ js/js1_homework/index.js
@@ -0,0 +1,23 @@
+
+//1-masala
+console.log('1-masala yechimlari:         ')
+let a=5,b=6,c=7
+let S=0.25*(((a+b-c)*(a-b+c)*(-a+b+c)*(a+b+c))**0.5)
+console.log('Uchburchakning yuzi: '+S)
+
+
+//2-masala
+console.log('2-masala yechimlari: ')
+let a1=20,b1=40
+let yuzaTortburchak=a1*b1
+console.log('To"g"ri turtburchak yuzasi: '+yuzaTortburchak)
+
+//3-masala
+console.log('3-masala yechimlari:  ')
+let n1=4,n2=5
+console.log('yig"indisi:'+(n1+n2),',ko"paytmasi: '+n1*n2,',n1 ning kvadrati: '+n1**2,',n2 ning kvadrati: '+n2**2)
+
+//4-masala
+console.log('4-masala yechimlari:  ')
+let son1=6,son2=7
+console.log('orta arifmitigi: '+(son1+son2)/2)

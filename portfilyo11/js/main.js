be504f495d49b02e1dfb304e95caba877f2a72af
Anvarbek <anvarbektoshmamatov888@gmail.com>
2023-06-14 14:29:41 +0500

    finish

---
+++ portfilyo11/js/main.js
@@ -0,0 +1,32 @@
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    grabCursor:true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+
+
+
+
+
+
+// function foo(...numbers){ 
+//   console.log(numbers) 
+// }
+//  foo(1,2,3,4,5)  
+
+
+// const arr=[1,2,3,4]
+// console.log(arr.reverse())
+
+
